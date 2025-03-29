ad6d4ae4f82ce0288fdbfcbda87ee8096f2a14eb
Joao <joaojoaocarlos045@gmail.com>
2025-03-29 20:57:40 -0300

    projeto finalizado

--- src/main/java/com/senai/task/repositories/TaskRepository.java
+++ src/main/java/com/senai/task/repositories/TaskRepository.java
@@ -4,13 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<TaskModel, Long> {
 
-    //
     boolean existsByUsuarioModel_Email(String email);
 
     Optional<TaskModel> findByDataAgendamentoAndUsuarioModel_Email(LocalDate dataAgendamento, String emailUsuario);
